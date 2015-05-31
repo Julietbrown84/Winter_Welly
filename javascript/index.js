2929805339f47880bebc4f71a05e27bc8fa9d2fc
Macintoshi HD <MacBook@Macintoshis-MBP.station>
2015-05-31 17:53:19 +1200

    changed background image

--- javascript/index.js
+++ javascript/index.js
@@ -3,27 +3,30 @@ $(window).ready(function() {
   $("h1").animate({top: '300px',right: '300px'});
  });
 
-   function fallingSnow() {
-    
-        var snowflake = $('<div class="snowflakes"></div>');
-        $('#snowZone').prepend(snowflake);
-        snowX = Math.floor(Math.random() * $('#site').width());
-        snowSpd = Math.floor(Math.random() + 5000);
+function fallingSnow() {
 
-        snowflake.css({'left':snowX+'px'});
-        snowflake.animate({
-            top: "500px",
-            opacity : "0",
+  var snowflake = $('<div class="snowflakes"></div>');
+  $('#snowZone').prepend(snowflake);
+  snowX = Math.floor(Math.random() * $('#site').width());
+  snowSpd = Math.floor(Math.random() + 5000);
 
-        }, snowSpd, function(){
-            $(this).remove();
-            fallingSnow();
-        });
+  snowflake.css({'left':snowX+'px'});
+  snowflake.animate({
+      top: "500px",
+      opacity : "0",
 
-    }
-    var timer = Math.floor(Math.random() +1000);
-    
-    window.setInterval(function(){
-        fallingSnow();
-    }, timer);
+  }, snowSpd, function(){
+      $(this).remove();
+      fallingSnow();
+  });
+
+}
+var timer = Math.floor(Math.random() +1000);
+
+window.setInterval(function(){
+  fallingSnow();
+}, timer);
+
+
+
 $(document).ready(main);
